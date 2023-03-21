540c2e91a1dbb54a2814c26fba97bbb0cd9ca622
amen-ramadan <125081631+amen-ramadan@users.noreply.github.com>
2023-03-21 13:38:55 +0300

    checkout finish

--- src/index.js
+++ src/index.js
@@ -1,26 +1,80 @@
-import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
-import 'jquery/dist/jquery.min';
-import 'popper.js/dist/popper.min';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './css/style.css';
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import "jquery/dist/jquery.min";
+import "popper.js/dist/popper.min";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./css/style.css";
 
-import '@fortawesome/fontawesome-free/js/all.min';
+import "@fortawesome/fontawesome-free/js/all.min";
 
-$(function() {
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 
-    $('[data-toggle="tooltip"]').tooltip();
+  $(".add-to-cart-btn").click(function () {
+    alert("أضيف المُنتج إلى عربة الشراء");
+  });
 
-    $('.add-to-cart-btn').click(function() {
-        alert('أضيف المُنتج إلى عربة الشراء');
+  $("#copyright").text(
+    " جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear()
+  );
+
+  // تغيير العنصر active بين العناصر لتنتقل معه تنسيقاته { صفحة المنتج الالوان والاحجام}
+
+  $(".product-option input[type='radio']").change(function () {
+    $(this).parents(".product-option").siblings().removeClass("active");
+    $(this).parents(".product-option").addClass("active");
+  });
+
+
+  $('[data-remove-from-cart]').on( "click",function() { 
+    $(this).parents('[data-product-info]').remove();
+
+    // أعد حساب السعر الإجمالي بعد حذف أحد المُنتجات
+    calculateTotalPrice();
+});
+
+
+
+  // عندما تتغير كمية المنتج
+  $("[data-product-quantity]").on("change", function () {
+    // اجلب الكمية الجديدة
+    var newQuantity = $(this).val();
+
+    // ابحث عن السّطر الّذي يحتوي معلومات هذا المُنتج
+    var $parent = $(this).parents("[data-product-info]");
+
+    // اجلب سعر القطعة الواحدة من معلومات المنتج
+    var pricePerUnit = $parent.attr("data-product-price");
+
+    // السعر الإجمالي للمنتج هو سعر القطعة مضروبًا بعددها
+    var totalPriceForProduct = newQuantity * pricePerUnit;
+
+    // عين السعر الجديد ضمن خليّة السّعر الإجمالي للمنتج في هذا السطر
+    $parent.find(".total-price-for-product").text(totalPriceForProduct + "$");
+
+    // حدث السعر الإجمالي لكل المُنتجات
+    calculateTotalPrice();
+  });
+
+  function calculateTotalPrice() {
+    // أنشئ متغيّرًا جديدًا لحفظ السعر الإجمالي
+    var totalPriceForAllProducts = 0;
+
+    // لكل سطر يمثل معلومات المُنتج في الصّفحة
+    $("[data-product-info]").each(function () {
+      // اجلب سعر القطعة الواحدة من الخاصّية الموافقة
+      var pricePerUnit = $(this).attr("data-product-price");
+
+      // اجلب كمية المنتج من حقل اختيار الكمية
+      var quantity = $(this).find("[data-product-quantity]").val();
+
+      var totalPriceForProduct = pricePerUnit * quantity;
+
+      // أضف السعر الإجمالي لهذا المنتج إلى السعر الإجمالي لكل المُنتجات، واحفظ القيمة في المتغير نفسه
+      totalPriceForAllProducts =
+        totalPriceForAllProducts + totalPriceForProduct;
     });
 
-    $('#copyright').text(" جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear());
-
-    // تغيير العنصر active بين العناصر لتنتقل معه تنسيقاته { صفحة المنتج الالوان والاحجام}
-
-    $(".product-option input[type='radio']").change(function() {
-        $(this).parents('.product-option').siblings().removeClass('active');
-        $(this).parents('.product-option').addClass('active');
-    });
-
+    // حدث السعر الإجمالي لكل المُنتجات في الصفحة
+    $("#total-price-for-all-products").text(totalPriceForAllProducts + "$");
+  }
 });
